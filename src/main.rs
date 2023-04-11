e91463b961d62ab17da4d2a2d4cf9083f059b463
0skar16 <51135536+0skar16@users.noreply.github.com>
2023-04-12 01:11:33 +0200

    update start config and proc macro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bary::{Server};
-#[bary::bary_app]
+#[bary::bary_app(secret_key = "VGhpcyBpcyBhIHBsYWNlIGZvciB5b3VyIGFkISBMT0w=")]
 pub fn load(bary: Server) {
     bary.start().expect("Server errored");
 }
