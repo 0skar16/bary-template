791e42c9379cd1b27dd239e04480e574c3d6657a
0skar16 <51135536+0skar16@users.noreply.github.com>
2023-04-12 02:26:11 +0200

    fixed it!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bary::{Server};
+use bary::Server;
 #[bary::bary_app(secret_key = "VGhpcyBpcyBhIHBsYWNlIGZvciB5b3VyIGFkISBMT0w=")]
 pub fn load(bary: Server) {
     bary.start().expect("Server errored");
